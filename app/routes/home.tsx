0e01df67a04621913a0152c285086b43ce1bb3b1
Ahmed Missouri <ahmedmissouri@icloud.com>
2025-08-13 12:56:34 +0100

    added authentication using puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,25 @@
+import { useEffect } from 'react';
 import { resumes } from '../../constants';
 import type { Route } from './+types/home';
 import Navbar from '~/Components/Navbar';
 import ResumeCard from '~/Components/ResumeCard';
+import { usePuterStore } from '~/lib/puter';
+import { useNavigate } from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'CVisionary' }, { name: 'description', content: 'Feedback on your CV for your dream job.' }];
 }
 
 export default function Home() {
+  const { isLoading, auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate('/auth?next=/');
+    }
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.png')] bg-cover">
       <Navbar />
